8957a50d1f84d19654f552520c1be8f298d89c4f
Endre Márk Borza <endremborza@gmail.com>
2021-03-15 01:36:29 +0100

    init package

--- colassigner/tests/test_init.py
+++ colassigner/tests/test_init.py
@@ -1,5 +1,18 @@
-from colassigner import __version__
+import pandas as pd
+from colassigner import __version__, ColAssigner
 
 
 def test_import():
     assert isinstance(__version__, str)
+
+def test_assigner():
+
+    class Cols1(ColAssigner):    
+        def var1(self, df):
+            return df.iloc[:,0].mean()
+
+    _df = pd.DataFrame({"a": [1,2,3]}).assign(**Cols1())
+
+    assert (_df.loc[:, "var1"] == 2).all()
+    assert (_df.loc[:, Cols1.var1] == 2).all()
+    assert len(Cols1()) == 1
