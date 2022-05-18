1c5a43a177d1c8fd4ad2f37b6c9430959446a4fd
Endre Márk Borza <endremborza@gmail.com>
2022-05-18 10:33:03 +0200

    don't mess with lowercase names

--- colassigner/util.py
+++ colassigner/util.py
@@ -5,6 +5,8 @@ nonswitch_rex = re.compile("[0-9]{2}")
 
 
 def camel_to_snake(cc_str):
+    if cc_str == cc_str.lower():
+        return cc_str
     out = cc_str
     for group in wswitch_rex.findall(cc_str):
         if nonswitch_rex.findall(group):
